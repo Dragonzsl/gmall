838ad3573d7c7d3baea603712bde77cccb3407a0
zhushilin <g1335026358@gmail.com>
2022-03-02 19:48:30 +0800

    添加会员服务

---
+++ gulimall-member/src/main/java/com/shilin/gulimall/member/vo/SocialUser.java
@@ -0,0 +1,17 @@
+package com.shilin.gulimall.member.vo;
+
+import lombok.Data;
+
+/**
+ * @author shilin
+ * @email g1335026358@gmail.com
+ * @date 2020-11-21 18:21:09
+ */
+@Data
+public class SocialUser {
+    private String access_token;
+    private String remind_in;
+    private long expires_in;
+    private String uid;
+    private String isRealName;
+}
---
+++ gulimall-member/src/main/java/com/shilin/gulimall/member/vo/UserRegisterVo.java
@@ -0,0 +1,18 @@
+package com.shilin.gulimall.member.vo;
+
+import lombok.Data;
+
+/**
+ * @author shilin
+ * @email g1335026358@gmail.com
+ * @date 2020-11-19 18:29:22
+ */
+@Data
+public class UserRegisterVo {
+
+    private String userName;
+
+    private String password;
+
+    private String phoneNum;
+}
