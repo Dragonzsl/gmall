4b2ec893f795b9f80b52b6d6db70da4e9658f693
zhushilin <g1335026358@gmail.com>
2022-03-19 22:49:00 +0800

    添加订单服务

---
+++ gulimall-order/src/main/java/com/shilin/gulimall/order/constant/OrderConstant.java
@@ -0,0 +1,10 @@
+package com.shilin.gulimall.order.constant;
+
+/**
+ * @author shilin
+ * @email g1335026358@gmail.com
+ * @date 2020-12-01 12:26:27
+ */
+public class OrderConstant {
+    public static final String USER_ORDER_TOKEN_PREFIX = "order:token:";
+}
